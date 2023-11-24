95297210ad03d0e10411a52f28ab3f3c34e6f072
Rulep15 <lucasrulep@gmail.com>
2023-11-24 16:24:24 -0300

    README

--- compras/pedido/productos_control.php
+++ compras/pedido/productos_control.php
@@ -11,9 +11,12 @@ $unidad = $_REQUEST['vidum'];
 $descripcion = $_REQUEST['vdescripcion'];
 $precioc = $_REQUEST['vprecioc'];
 $preciov = $_REQUEST['vpreciov'];
+$pedi = $_REQUEST['vpedi'];
 $codigob = $_REQUEST['vcodigob'];
 
-$sql = "SELECT sp_ref_producto(" . $operacion . "," .
+
+
+$sql = "SELECT sp_ref_det_producto(" . $operacion . "," .
     (!empty($codigo) ? $codigo : 0) . "," .
     (!empty($tipopro) ? $tipopro : 0) . "," .
     (!empty($marca) ? $marca : 0) . "," .
@@ -22,13 +25,15 @@ $sql = "SELECT sp_ref_producto(" . $operacion . "," .
     (!empty($descripcion) ? $descripcion : "VACIO") . "'," .
     (!empty($precioc) ? $precioc : 0) . "," .
     (!empty($preciov) ? $preciov : 0) . "," .
+    (!empty($pedi) ? $pedi : 0) . "," .
     (!empty($codigob) ? $codigob : 0) . ") AS productos;";
 $resultado = consultas::get_datos($sql);
 
-if ($resultado[0]['dpedidosc'] != NULL) {
-    $_SESSION['mensaje'] = $resultado[0]['dpedidosc'];
-    header("location:pedidosc_detalle.php?vidpedido=" . $_REQUEST['vidpedido']);
+if ($resultado[0]['pedidosc'] != NULL) {
+    $valor = explode("*", $resultado[0]['pedidosc']);
+    $_SESSION['mensaje'] = $valor[0];
+    header("location:" . $valor[1]);
 } else {
     $_SESSION['mensaje'] = 'Error:' . $sql;
-    header("location:pedidosc_detalle.php?vidpedido=" . $_REQUEST['vidpedido']);
+    header("location:pedidosc_detalle.php");
 }
