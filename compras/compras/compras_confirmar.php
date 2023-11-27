8223a6690e7ff250741195492c1957e642e7a8e9
Rulep15 <lucasrulep@gmail.com>
2023-11-26 23:05:10 -0300

    README

--- compras/compras/compras_confirmar.php
+++ compras/compras/compras_confirmar.php
@@ -14,7 +14,7 @@ if (isset($_REQUEST['vidcompra'])) {
         <form action="compras_detalle_control.php" method="post" accept-charset="utf-8" class="form-horizontal">
 
             <h4 class="modal-title" style="text-align: center"><strong>¿Desea confirmar la Compra?</strong></h4>
-            <input name="voperacion" value="3" type="hidden">
+            <input name="voperacion" value="4" type="hidden">
             <input class="form-control" name="vidcompra" type="hidden" value="<?php echo $gru ?>" readonly="" id="codigo" onkeypress="return soloNum(event)" required="">
             <div class="modal-footer" style="border-top: 1px solid #e5e5e5;margin-left: -1.1em;margin-right: -1.1em;margin-top: 1.5em;;padding-top: 1em;padding-right: 1em;">
                 <button type="submit" class="btn btn-success pull-left">
